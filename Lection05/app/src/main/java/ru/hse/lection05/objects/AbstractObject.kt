0f76b002b9a230d58f9b5e86983269bad40ea1ed
RUTUBE\nkleshchin <nkleshchin@uma.tech>
2021-04-15 09:10:20 +0300

    deleted module for fixing leading char

--- Lection05/app/src/main/java/ru/hse/lection05/objects/AbstractObject.kt
+++
@@ -1,6 +0,0 @@
-package ru.hse.lection05.objects
-
-import java.io.Serializable
-
-abstract class AbstractObject: Serializable {
-}
--- Lection05/app/src/main/java/ru/hse/lection05/presentationlayer/views/IPlaceView.kt
+++
@@ -1,5 +0,0 @@
-package ru.hse.lection05.presentationlayer.views
-
-interface IPlaceView {
-    fun nextStep()
-}
