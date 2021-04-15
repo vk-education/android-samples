1f9770d1ab16e601aef01f87d75c2dd1ca8a660f
RUTUBE\nkleshchin <nkleshchin@uma.tech>
2021-04-15 09:10:20 +0300

    deleted module for fixing leading char

--- Lection05/app/src/main/java/ru/hse/lection05/presentationlayer/adapters/calculators/PlaceCalculator.kt
+++
@@ -1,9 +0,0 @@
-package ru.hse.lection05.presentationlayer.adapters.calculators
-
-import ru.hse.lection05.objects.Place
-
-class PlaceCalculator: SimpleCalculator<Place>() {
-    override fun areItemsTheSame(oldItem: Place, newItem: Place): Boolean {
-        return oldItem.id == newItem.id
-    }
-}
