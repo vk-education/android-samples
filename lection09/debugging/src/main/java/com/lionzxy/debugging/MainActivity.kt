525ddda1061758268533d1a4f5fceba4ef18097f
LionZXY <nikita@kulikof.ru>
2021-05-19 17:43:33 +0300

    Prepare to lection09

---
+++ lection09/debugging/src/main/java/com/lionzxy/debugging/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lionzxy.debugging
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.snackbar.Snackbar
+import com.lionzxy.debugging.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        getSharedPreferences("test", MODE_PRIVATE).edit().putString("test", "Yay!").apply()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
